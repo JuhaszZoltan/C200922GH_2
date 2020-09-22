729e3cb18a59535595861e0cc1b966337df2bfa6
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-09-22 10:26:06 +0200

    add 100 *

--- C200922GH_2/Program.cs
+++ C200922GH_2/Program.cs
@@ -4,11 +4,19 @@ namespace C200922GH_2
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main()
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Juhász Zoltán");
-            Console.WriteLine("fsdfgdf");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)rnd.Next(1, 16);
+                Console.SetCursorPosition(
+                    rnd.Next(Console.WindowWidth),
+                    rnd.Next(Console.WindowHeight));
+                Console.Write("*");
+            }
             Console.ReadKey(true);
         }
     }
